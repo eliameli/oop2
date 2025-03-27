af92ef3111e26ec5f272633b2ac5fdcb4f471d8e
eliameli <shishkacone007@gmail.com>
2025-03-27 23:24:25 +0300

    commit8 что то новое

--- app/src/main/java/com/example/oop2/models/DiskType.kt
+++ app/src/main/java/com/example/oop2/models/DiskType.kt
@@ -1,5 +1,13 @@
 package com.example.oop2.models
+import com.example.oop2.models.LibraryItem
 
-enum class DiskType {
-    CD, DVD
-}
+enum class DiskType { CD, DVD }
+
+
+
+data class LibraryItem(
+    val id: Int,
+    val name: String,
+    var isAvailable: Boolean,
+    val iconResId: Int // id dlya ikonki
+)
