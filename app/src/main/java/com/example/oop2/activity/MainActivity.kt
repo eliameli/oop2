d352c9f9be462289b594af633755e5033d42d6cb
eliameli <shishkacone007@gmail.com>
2025-04-14 21:19:56 +0300

    commit21 вроде норм 8.2

--- app/src/main/java/com/example/oop2/activity/MainActivity.kt
+++ app/src/main/java/com/example/oop2/activity/MainActivity.kt
@@ -1,6 +1,4 @@
-// MainActivity.kt
 package com.example.oop2.activity
-
 import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -8,14 +6,11 @@ import com.example.oop2.R
 import com.example.oop2.fragments.DetailsFragment
 import com.example.oop2.fragments.ListFragment
 import com.example.oop2.models.LibraryItem
-
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity(), ListFragment.OnItemClickListener {
-
     @Deprecated("This method has been deprecated in favor of using the\n      {@link OnBackPressedDispatcher} via {@link #getOnBackPressedDispatcher()}.\n      The OnBackPressedDispatcher controls how back button events are dispatched\n      to one or more {@link OnBackPressedCallback} objects.")
     override fun onBackPressed() {
         val isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
-
         if (isLandscape) {
             val detailsFragment = supportFragmentManager.findFragmentById(R.id.fragment_details_container)
             if (detailsFragment != null) {
@@ -32,38 +27,54 @@ class MainActivity : AppCompatActivity(), ListFragment.OnItemClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tag = "details"
         val isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
-        supportFragmentManager.findFragmentById(R.id.fragment_container)?.let {
-            supportFragmentManager.beginTransaction().remove(it).commitNow()
+        val existingDetailsArgs = supportFragmentManager.findFragmentByTag(tag)?.arguments
+        val existingDetails = existingDetailsArgs?.let {
+            DetailsFragment().apply { arguments = it }
         }
-
+        //удаление фрагментов при переходе в ландскейп
         if (isLandscape) {
-            val hasList = supportFragmentManager.findFragmentById(R.id.fragment_list_container) != null
-
-            if (!hasList) {
+            supportFragmentManager.findFragmentById(R.id.fragment_container)?.let {
+                supportFragmentManager.beginTransaction().remove(it).commitNow()
+            }
+        }
+        if (isLandscape) {
+            // альбомная ариентация
+            if (supportFragmentManager.findFragmentById(R.id.fragment_list_container) == null) {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_list_container, ListFragment())
                     .commit()
             }
+            if (existingDetails is DetailsFragment) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_details_container, existingDetails, tag)
+                    .commit()
+            }
         } else {
-            val listFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
-            if (listFragment !is ListFragment) {
+            // портретка
+            if (existingDetails is DetailsFragment) {
+                supportFragmentManager.popBackStack(null, androidx.fragment.app.FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, existingDetails, tag)
+                    .addToBackStack(null)
+                    .commit()
+            } else {
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, ListFragment())
                     .commit()
             }
         }
     }
-
     override fun onItemClicked(item: LibraryItem?) {
         val isLandscape = resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE
         val fragment = DetailsFragment.newInstance(item)
+        val tag = "details"
 
         supportFragmentManager.beginTransaction()
             .replace(
                 if (isLandscape) R.id.fragment_details_container else R.id.fragment_container,
-                fragment
-            )
+                fragment, tag)
             .apply {
                 if (!isLandscape) addToBackStack(null)
             }
